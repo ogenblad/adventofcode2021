d4642d2f93113b87d4503fb8fc38508ea4c913fb
Simon Ogenblad <simon@ogenblad.se>
2021-12-04 14:22:29 +0100

    creating grids

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _04
 {
@@ -6,7 +9,51 @@ namespace _04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllLines("test.txt");
+            var numbers = input[0].Split(",").Select(Int32.Parse).ToList();
+            BuildGrids(input);
         }
+
+        public static void BuildGrids(string[] input)
+        {
+            int currentRow = 0;
+            Grid tempGrid  = new Grid { Rows = new List<List<Tile>>() };
+
+            for ( var i=2; i < input.Length; i++)
+            {
+                if (currentRow == 0)
+                {
+                    tempGrid = new Grid();
+                }
+
+                input[i] = input[i].Replace("  ", " ");
+                var currentRowNumbers = input[i].Split(" ").Select(Int32.Parse).ToArray();
+                tempGrid.Rows.Add(new List<Tile>());
+                foreach (var number in currentRowNumbers)
+                {
+                    tempGrid.Rows.LastOrDefault().Add(new Tile { Value = number, IsMarked = false});
+                }
+
+                currentRow++;
+
+                if (currentRow > 4)
+                {
+                    currentRow = 0;
+                    i++;
+                }
+
+            }
+        }
+    }
+
+    public class Tile
+    {
+        public int Value { get; set; }
+        public bool IsMarked { get; set; }
+    }
+
+    public class Grid
+    {
+        public IList<List<Tile>> Rows { get; set; }
     }
 }
