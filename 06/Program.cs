fc832c826de4a22970eb662c4ecdd05d68e694e5
Simon Ogenblad <simon.ogenblad@iver.se>
2021-12-08 22:49:55 +0100

    finnish day 6

--- 06/Program.cs
+++ 06/Program.cs
@@ -11,93 +11,29 @@ namespace _06
         {
             var input = File.ReadAllText("input.txt");
             var numbers = input.Split(",").Select(Int32.Parse).ToList();
-            var fishyResult = PopulateFishes(numbers, 80);
-
-            Console.WriteLine($"Answer: {fishyResult.Fishes.Count()}");
-            Console.WriteLine("Log:");
-            fishyResult.Log.ForEach(i => Console.WriteLine(i));
-        }
-
-        public static FishResult PopulateFishes(List<int> numbers, int numberOfDays)
-        {
-            List<Fish> fishes = new List<Fish>();
-            List<string> log = new List<string>();
-            int counter = 0;
-
-            log.Add($"Initial state: {string.Join(",", numbers)}");
             
-            foreach (var number in numbers)
+            Console.WriteLine($"Assigment1: {CountFishes(numbers, 80)}");
+            Console.WriteLine($"Assigment1: {CountFishes(numbers, 256)}");
+        }
+
+        public static long CountFishes(List<int> initial, int numberOfDays)
+        {
+            List<long> state = new List<long> {0,0,0,0,0,0,0,0,0};
+            
+            foreach (var currentInitialState in initial)
             {
-                fishes.Add(new Fish { Timer = number});
+                state[currentInitialState]++;
             }
 
-            for (var d = 0; d < numberOfDays; d++)
+            for (var currentDay = 0; currentDay < numberOfDays; currentDay++)
             {
-                List<Fish> newBornFishes = new List<Fish>();
-                
-                foreach ( var fish in fishes)
-                {
-                    var createNewFish = fish.CountDay();
-                    
-                    if (createNewFish)
-                    {
-                        newBornFishes.Add(new Fish());
-                    }
-                }
-
-                if (counter > 0)
-                {
-                    string currentState = string.Join(",", fishes.Select(f => f.Timer).ToList());
-                    log.Add($"After {counter} days: {currentState}");
-                }
-                
-                fishes = fishes.Concat(newBornFishes).ToList();
-                counter++;
+                state[7] += state[0];
+                state.Insert(9, state[0]);
+                state.RemoveAt(0);
             }
 
-            return new FishResult { Fishes = fishes, Log = log };
+            long sum = state.Sum();
+            return sum;
         }
-    }
-
-    public class Fish
-    {
-        public int Timer { get; set; }
-        public int Children { get; set; }
-
-        public Fish (int timer = 8, int children = 0)
-        {
-            this.Timer = timer;
-            this.Children = children;
-        }
-
-        private void GiveBirth()
-        {
-            this.Timer = 6;
-            this.Children++;
-        }
-
-        public int ChildrenCount()
-        {
-            return this.Children;
-        }
-
-        public bool CountDay()
-        {
-            this.Timer--;
-
-            if (this.Timer < 0)
-            {
-                this.GiveBirth();
-                return true;
-            }
-
-            return false;
-        }
-    }
-
-    public class FishResult
-    {
-        public List<Fish> Fishes { get; set; }
-        public List<string> Log { get; set; }
     }
 }
