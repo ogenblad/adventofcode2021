f6ce1797be424358052067fcec4c241a3538d41d
Simon Ogenblad <simon@ogenblad.se>
2021-12-10 00:35:24 +0100

    refactor 07

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -19,7 +18,7 @@ namespace _07
             sw.Stop();
 
             Console.WriteLine($"TASK1: Least amount of consumed fuel: {result[0].Item1}, target position: {result[0].Item2}");
-            Console.WriteLine($"TASK1: Least amount of consumed fuel: {result[1].Item1}, target position: {result[1].Item2}");
+            Console.WriteLine($"TASK2: Least amount of consumed fuel: {result[1].Item1}, target position: {result[1].Item2}");
             Console.WriteLine($"Execution time: {sw.Elapsed.TotalMilliseconds}");
         }
 
@@ -34,38 +33,24 @@ namespace _07
                 initialPositionCount.Add(Tuple.Create(key.Key, count));
             }
 
-            List<Tuple<int, int>> fuelResult1 = new List<Tuple<int, int>>();
-            List<Tuple<int, int>> fuelResult2 = new List<Tuple<int, int>>();
+            List<Tuple<int, int>> fuelResultCheap = new List<Tuple<int, int>>();
+            List<Tuple<int, int>> fuelResultExpensive = new List<Tuple<int, int>>();
             foreach (var position in initialPositionCount)
             {
-                int consumedFuel1 = 0;
-                int consumedFuel2 = 0;
-                foreach (var move in initialPositionCount)
-                {
-                    consumedFuel1 += CalculateFuelConsumption(move.Item1, position.Item1, move.Item2, false);
-                    consumedFuel2 += CalculateFuelConsumption(move.Item1, position.Item1, move.Item2, true);
-                }
-
-                fuelResult1.Add(Tuple.Create(consumedFuel1, position.Item1));
-                fuelResult2.Add(Tuple.Create(consumedFuel2, position.Item1));
+                fuelResultCheap.Add(CalculateFuelConsumtionList(initialPositionCount, position.Item1, false));
+                fuelResultExpensive.Add(CalculateFuelConsumtionList(initialPositionCount, position.Item1, true));
             }
 
-            var indexDiff = fuelResult2.OrderBy(f => f.Item1).Take(2).ToList();
+            var additionalTargets = fuelResultExpensive.OrderBy(f => f.Item1).Take(2).ToList();
 
-            for (var i = indexDiff.FirstOrDefault().Item2+1; i < indexDiff.Last().Item2; i++)
+            for (var additionalTarget = additionalTargets.FirstOrDefault().Item2+1; additionalTarget < additionalTargets.Last().Item2; additionalTarget++)
             {
-                int consumedFuel2 = 0;
-                foreach (var move in initialPositionCount)
-                {
-                    consumedFuel2 += CalculateFuelConsumption(move.Item1, i, move.Item2, true);
-                }
-
-                fuelResult2.Add(Tuple.Create(consumedFuel2, i));
+                fuelResultExpensive.Add(CalculateFuelConsumtionList(initialPositionCount, additionalTarget, true));
             }
 
             var result = new List<Tuple<int, int>>();
-            result.Add(fuelResult1.OrderBy(f => f.Item1).FirstOrDefault());
-            result.Add(fuelResult2.OrderBy(f => f.Item1).FirstOrDefault());
+            result.Add(fuelResultCheap.OrderBy(f => f.Item1).FirstOrDefault());
+            result.Add(fuelResultExpensive.OrderBy(f => f.Item1).FirstOrDefault());
             
             return result;
         }
@@ -76,14 +61,25 @@ namespace _07
             return input;
         }
 
-        public static int CalculateFuelConsumption(int currentLocation, int targetLocation, int count, bool expensive)
+        public static Tuple<int, int> CalculateFuelConsumtionList(List<Tuple<int, int>> input, int targetPosition, bool expensive)
+        {
+            int consumedFuel = 0;
+            foreach (var move in input)
+            {
+                consumedFuel += CalculateFuelConsumption(move.Item1, targetPosition, move.Item2, expensive);
+            }
+
+            return Tuple.Create(consumedFuel, targetPosition);
+        }
+
+        public static int CalculateFuelConsumption(int currentPosition, int targetPosition, int count, bool expensive)
         {
             if (!expensive)
             {
-                return currentLocation != targetLocation ? EnsurePositive((currentLocation - targetLocation) * count) : 0;
+                return currentPosition != targetPosition ? EnsurePositive((currentPosition - targetPosition) * count) : 0;
             }
             
-            return currentLocation != targetLocation ? (Enumerable.Range(1,EnsurePositive(currentLocation - targetLocation)).Sum()) * count : 0;;
+            return currentPosition != targetPosition ? (Enumerable.Range(1,EnsurePositive(currentPosition - targetPosition)).Sum()) * count : 0;;
         }
     }
 }
