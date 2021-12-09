e9e63a21176ff397f45b9a3907d4fa10514100b1
Simon Ogenblad <simon.ogenblad@iver.se>
2021-12-09 17:42:32 +0100

    Init day 07

---
+++ 07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            
+            var input = File.ReadAllText("input.txt");
+            var numbers = input.Split(",").Select(Int32.Parse).ToList();
+        }
+    }
+}
