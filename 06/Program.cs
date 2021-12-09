3153aab9fa3cbf15a0014dbee65e190b79f7a860
Simon Ogenblad <simon.ogenblad@iver.se>
2021-12-09 14:32:10 +0100

    added stopwatch

--- 06/Program.cs
+++ 06/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -12,8 +13,21 @@ namespace _06
             var input = File.ReadAllText("input.txt");
             var numbers = input.Split(",").Select(Int32.Parse).ToList();
             
-            Console.WriteLine($"Assigment1: {CountFishes(numbers, 80)}");
-            Console.WriteLine($"Assigment1: {CountFishes(numbers, 256)}");
+            Stopwatch sw1 = new Stopwatch();
+            Stopwatch sw2 = new Stopwatch();
+            
+            sw1.Start();
+            long a1 = CountFishes(numbers, 80);
+            sw1.Stop();
+
+            sw2.Start();
+            long a2 = CountFishes(numbers, 256);
+            sw2.Stop();
+
+
+            Console.WriteLine($"Assigment1: {a1}, {sw1.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Assigment1: {a2}, {sw2.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Total time: {sw1.ElapsedMilliseconds + sw2.ElapsedMilliseconds}ms");
         }
 
         public static long CountFishes(List<int> initial, int numberOfDays)
