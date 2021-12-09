0384bae6c486e257dbf152a03c818df2554c2ce6
Simon Ogenblad <simon.ogenblad@iver.se>
2021-12-09 21:33:36 +0100

    task1, day7 done

--- 07/Program.cs
+++ 07/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -10,10 +12,46 @@ namespace _07
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-            //sw.Start();
-            
+            sw.Start();
             var input = File.ReadAllText("input.txt");
-            var numbers = input.Split(",").Select(Int32.Parse).ToList();
+            var positions = input.Split(",").Select(Int32.Parse).ToList();
+            var result = CalculateFuelConsumption(positions);
+            sw.Stop();
+
+            Console.WriteLine($"Least amount of consumed fuel: {result.Item1}, target position: {result.Item2}");
+            Console.WriteLine($"Execution time: {sw.Elapsed.TotalMilliseconds}");
+        }
+
+        public static Tuple<int, int> CalculateFuelConsumption(List<int> input)
+        {
+            List<Tuple<int, int>> initialPositionCount = new List<Tuple<int, int>>();
+            var groupedPositions = input.GroupBy(p => p).ToList();
+
+            foreach (var key in groupedPositions)
+            {
+                var count = key.Select(x => x).Count();
+                initialPositionCount.Add(Tuple.Create(key.Key, count));
+            }
+
+            List<Tuple<int, int>> fuelResult = new List<Tuple<int, int>>();
+            foreach (var position in initialPositionCount)
+            {
+                int consumedFuel = 0;
+                foreach (var move in initialPositionCount)
+                {
+                    consumedFuel += move.Item1 != position.Item1 ? EnsurePositive((move.Item1 - position.Item1) * move.Item2) : 0;
+                }
+
+                fuelResult.Add(Tuple.Create(consumedFuel, position.Item1));
+            }
+
+            return fuelResult.OrderBy(f => f.Item1).FirstOrDefault();
+        }
+
+        public static int EnsurePositive(int input)
+        {
+            if (input < 0) { return input * -1; }
+            return input;
         }
     }
 }
